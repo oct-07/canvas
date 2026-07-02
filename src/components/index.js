7cfa5082a99aea1653774667e39faf483d976efe
Octopus <2909124929@qq.com>
2026-07-02 11:07:38 +0800

    项目规范

--- src/components/index.js
+++ src/components/index.js
@@ -3,15 +3,12 @@
  */
 
 // Canvas 画布组件
-import ContextMenu from './Canvas/ContextMenu'
-import CustomNode from './Canvas/CustomNode'
-import Canvas from './Canvas/index'
-import SideBar from './Canvas/SideBar'
-
-
-
-
+import ContextMenu from "./Canvas/ContextMenu";
+import CustomEdge from "./Canvas/CustomEdge";
+import CustomNode from "./Canvas/CustomNode";
+import CustomPoint from "./Canvas/CustomPoint";
+import Canvas from "./Canvas/index";
+import SideBar from "./Canvas/SideBar";
 
 // 统一具名导出
-export { Canvas, ContextMenu, CustomNode, SideBar }
-
+export { Canvas, ContextMenu, CustomEdge, CustomNode, CustomPoint, SideBar };
